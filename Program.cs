8bff188302873080445cc8b60f1d27c22ca1db4c
cihankaya04 <148449398+cihankaya04@users.noreply.github.com>
2024-01-10 10:59:18 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace programlama_temelleri_odev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            var kullanılan = new System.Collections.Generic.List<int>();
+
+            Console.Write("Kaç sayı almak istiyorsunuz? ");
+            int sayi = Convert.ToByte(Console.ReadLine());
+
+            for (int i = 0; i < sayi; i++)
+            {
+                int yeni = random.Next(1, 99);
+
+                while (kullanılan.Contains(yeni))
+                {
+                    yeni = random.Next(1, 99);
+                }
+                Console.WriteLine("yeni sayı:" + yeni);
+                kullanılan.Add(yeni);
+            }
+            Console.ReadKey();
+        }
+    }
+}
